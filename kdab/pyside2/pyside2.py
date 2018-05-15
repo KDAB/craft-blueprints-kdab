d2329b012ad457f31f2092c0818a5a98fef9c7e2
Hannah von Reth <hannah.vonreth@kdab.com>
2018-05-15 13:14:33 +0200

    Disable broken modules

--- kdab/pyside2/pyside2.py
+++ kdab/pyside2/pyside2.py
@@ -24,11 +24,28 @@ class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
         self.supportsNinja = False
+
+
+
+    def configureOptions(self, defines=""):
+        defines = super().configureOptions(defines)
         if ("Paths", "Python") in CraftCore.settings:
             python = os.path.join(CraftCore.settings.get("Paths", "Python"), f"python{CraftCore.compiler.executableSuffix}")
         if not os.path.exists(python):
             if CraftCore.compiler.isWindows:
                 CraftCore.log.warning(f"Could not find {python} as provided by [Paths]Python, using {sys.executable} as a fallback")
             python = sys.executable
-        self.subinfo.options.configure.args += f" -DPYTHON_EXECUTABLE=\"{python}\""
-        self.subinfo.options.configure.args += f" -DLLVM_CONFIG=\"{os.path.join(CraftCore.standardDirs.craftRoot(), 'bin', 'llvm-config')}\""
+        defines += f" -DPYTHON_EXECUTABLE=\"{python}\""
+        defines += f" -DLLVM_CONFIG=\"{os.path.join(CraftCore.standardDirs.craftRoot(), 'bin', 'llvm-config')}\""
+
+        disabledModules = ["WebKit", "WebKitWidgets"]
+
+        # Qt3d needs https://codereview.qt-project.org/#/c/219105/ so 5.10.2?
+        if CraftVersion(CraftPackageObject.get("libs/qt5/qtbase").version) < CraftVersion("5.10.2"):
+            disabledModules += ["3DCore", "3DRender", "3DInput", "3DLogic", "3DAnimation", "3DExtras"]
+
+        for p in disabledModules:
+            defines += f" -DCMAKE_DISABLE_FIND_PACKAGE_Qt5{p}=ON"
+
+        return defines
+
